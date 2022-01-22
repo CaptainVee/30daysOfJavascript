450b7c4476f27b58f468b0c4081b72701652e6c3
unknown <captainvee3@gmail.com>
2022-01-22 23:05:37 +0100

    listen for submit

--- 4_4_Loan_calculator/app.js
+++ 4_4_Loan_calculator/app.js
@@ -4,6 +4,7 @@ document.getElementById('loan-form').addEventListener("submit", calculateResults
 //Calculate result
 function calculateResults(e) {
     
+
     console.log("Calculating....")
     e.preventDefault()
 }
