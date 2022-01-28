50228ae936637c439a195be8bc211708719472b3
unknown <captainvee3@gmail.com>
2022-01-28 23:22:02 +0100

    added basic stuf to loan calculator

--- 4_4_Loan_calculator/loancalculator/app.js
+++
@@ -1,9 +0,0 @@
-//Listen for submit
-document.getElementById('loan-form').addEventListener("submit", calculateResults)
-
-//Calculate result
-function calculateResults(e) {
-    
-    console.log("Calculating....")
-    e.preventDefault()
-}
