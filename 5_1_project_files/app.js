5d6a5feaf9da3501174bb881be470b16eb0daad9
CaptainVee <captainvee3@gmail.com>
2022-07-16 22:11:33 +0100

    more on constructors

--- 5_1_project_files/app.js
+++ 5_1_project_files/app.js
@@ -1,8 +1,15 @@
 // Person constructor
-function Person(name, age){
+function Person(name, age, dob){
 	this.name = name
 	this.age = age
+	this.dob = new Date(dob)
+
+	this.CalculateAge = function(){
+		const diff = Date.now() - this.dob.getTime()
+		const ageDate = new Date(diff)
+		return Math.abs(ageDate.getUTCFullYear() - 1970)
+	}
 }
 
-const vee = new Person('vee', 29)
-console.log(vee.age)
+const vee = new Person('vee', 29, '9-10-1990')
+console.log(vee.CalculateAge())
