bb31d7e87122d4c07f350c1d96f4c4cee6794242
CaptainVee <captainvee3@gmail.com>
2022-07-17 22:50:01 +0100

    some stuffs i didn't understand

--- 5_1_project_files/app.js
+++ 5_1_project_files/app.js
@@ -1,6 +1,7 @@
 // Person constructor
-function Person(name, age, dob){
-	this.name = name
+function Person(firstname, lastname, age, dob){
+	this.firstname = firstname
+	this.lastname = lastname
 	this.age = age
 	this.dob = new Date(dob)
 
@@ -11,5 +12,18 @@ function Person(name, age, dob){
 	}
 }
 
-const vee = new Person('vee', 29, '9-10-1990')
-console.log(vee.CalculateAge())
+const vee = new Person('vee', 'joe', 29, '9-10-1990')
+const mary = new Person('mary', 'John', 59, '10-10-2003')
+
+// Get full name
+Person.prototype.getFullname = function() {
+	return `${this.firstname} ${this.lastname}`
+}
+
+// Gets married
+Person.prototype.getsMarried = function(newLastname){
+	this.lastname=newLastname
+}
+
+mary.getsMarried('cow')
+console.log(mary.getFullname())
