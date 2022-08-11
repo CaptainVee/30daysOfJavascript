1a0f81d8101732aa1649c202e91c200cf7253a0f
CaptainVee <captainvee3@gmail.com>
2022-08-11 22:31:42 +0100

    adding books to local storage

--- Book_List/app.js
+++ Book_List/app.js
@@ -55,6 +55,56 @@ class UI{
 	}
 }
 
+
+class Store{
+	static getBooks(){
+		let books
+		if(localStorage.getItem('books') === null ){
+			books = []
+		}else{
+			books = JSON.parse(localStorage.getItem('books'))
+		}
+
+		return books
+	}
+
+	static displayBooks(){
+		const books = Store.getBooks()
+
+		books.forEach(function(book){
+			const ui = new UI
+
+			// Add book to UI
+			ui.addBookToList(book)
+		})
+	}
+
+	static addBook(book){
+		const books = Store.getBooks()
+
+		books.push(book)
+		localStorage.setItem('books', JSON.stringify(books))
+
+	}
+
+	static removeBook(isbn){
+		const books = Store.getBooks()
+
+		books.forEach(function(book, index){
+			if(book.isbn === isbn ){
+				books.splice(index, 1)
+
+			}
+		})
+		localStorage.setItem('books', JSON.stringify(books))
+
+	}
+}
+
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
 // Event Listeners
 
 document.getElementById('book-form').addEventListener('submit',
@@ -80,6 +130,9 @@ document.getElementById('book-form').addEventListener('submit',
 		// Add book to list 
 		ui.addBookToList(book)
 
+		// Add to Local Storage
+		Store.addBook(book)
+
 		// show success
 		ui.showAlert('Book Added!', 'success')
 
@@ -97,7 +150,12 @@ document.getElementById('book-list').addEventListener('click', function(e){
 	const ui = new UI()
 	ui.deleteBook(e.target)
 
+	// Remove book from Local Storage
+	Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+
 	ui.showAlert('Book removed!', 'success')
 
 	e.preventDefault()
 })
+
+
