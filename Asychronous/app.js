e58045e01822091ec33fb3a8b1d7827cb5c6b103
CaptainVee <captainvee3@gmail.com>
2022-08-13 22:52:51 +0100

    nothing much

--- Asychronous/app.js
+++ Asychronous/app.js
@@ -7,6 +7,12 @@ function loadData(){
 	// Open
 	xhr.open('GET', 'data.txt', true)
 
+	//Optional - Used for spinners/loader while waiting for data to fetch
+
+	xhr.onprogress = function(){
+		console.log('READY STATE', xhr.readyState)
+	}
+
 	xhr.onload = function(){
 		if(this.status === 200){
 			console.log(this.responseText)
