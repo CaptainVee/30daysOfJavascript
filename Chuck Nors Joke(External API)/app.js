9c942262fe00a0d15c028b1f583f53d4dbe3e0a5
CaptainVee <captainvee3@gmail.com>
2022-08-16 22:50:54 +0100

    ore work

--- Chuck Nors Joke(External API)/app.js
+++ Chuck Nors Joke(External API)/app.js
@@ -2,7 +2,22 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes)
 
 function getJokes(e){
 	const number = document.querySelector('input[type="number"]').value
-	console.log(number)
+
+	xhr = new XMLHttpRequest()
+
+	xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true)
+
+	xhr.onload = function(){
+		if(this.status === 200){
+			const response = JSON.parse(this.responseText)
+			
+			let output = ''
+
+			if(response.type === 'success')
+		}
+	}
+
+	xhr.send()
 
 	e.preventDefault()
 
