048a6d031c41fc78b28f14ce01e96d8fa57c3849
CaptainVee <captainvee3@gmail.com>
2022-08-27 22:24:41 +0100

    easyhttp stuff

---
+++ callbacks and all/app.js
@@ -0,0 +1,26 @@
+const posts = [
+	{title:'Post one', body: 'This is the body of post one'},
+	{title:'Post two', body: 'This is the body of post two'}
+]
+
+function createPost(post, callback){
+
+	setTimeout(function(){
+		posts.push(post)
+		callback()
+	}, 2000)
+
+}
+
+function getPosts(){
+
+	setTimeout(function(){
+		let output = ''
+		posts.forEach(function(post){
+			output += `<li>${post.title}</li>`
+		})
+		document.body.innerHTML = output
+	}, 1000)
+}
+
+createPost({title:'Post three', body: 'This is the body of post three'}, getPosts)
