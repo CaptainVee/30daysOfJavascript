bc33de5891f51787e4dc7bb5526692ec1618275e
CaptainVee <captainvee3@gmail.com>
2022-08-29 22:20:26 +0100

    easyhttp es5 library completed

--- EasyHTTP/app.js
+++ EasyHTTP/app.js
@@ -1,12 +1,62 @@
+
 const http = new easyHTTP
 
 // Get posts
 
-http.get('https://jsonplaceholder.typicode.com/posts', 	function(err, response){
+http.get('https://jsonplaceholder.typicode.com/posts', 	function(err, posts){
 
 	if(err){ 
 		console.log(err)
 	}else{
-		console.log(response)
+		console.log(posts)
+	}
+})
+
+// Get single post
+
+http.get('https://jsonplaceholder.typicode.com/posts/1', 	function(err, post){
+
+	if(err){ 
+		console.log(err)
+	}else{
+		console.log(post)
+	}
+})
+
+
+// POST post
+
+const data = {
+	title: 'custom post',
+	body: 'custom body'
+}
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+	if(err){ 
+		console.log(err)
+	}else{
+		console.log(post)
+	}
+
+})
+
+// PUT post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+	if(err){ 
+		console.log(err)
+	}else{
+		console.log(post)
+	}
+
+})
+
+// DELETE post
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, posts){
+
+	if(err){ 
+		console.log(err)
+	}else{
+		console.log(posts)
 	}
 })
