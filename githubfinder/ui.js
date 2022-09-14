748e27f1ce5d8430ac46eb141f0aabc81ce04410
CaptainVee <captainvee3@gmail.com>
2022-09-14 22:49:34 +0100

    github finder finished and weather app start

--- githubfinder/ui.js
+++ githubfinder/ui.js
@@ -44,13 +44,20 @@ class UI{
 			  <div class="card card-body mb-2">
 			    <div class="row">
 			      <div class="col-md-6">
+			      	<a href="${repo.html_url}" target="_blank">${repo.name}</a>
 			      </div>
 			      <div class="col-md-6">
+					<span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
+					<span class="badge bg-secondary">Watchers: ${repo.watchers_count}</span>
+					<span class="badge bg-warning">Forks: ${repo.forks_count}</span>
 			      </div>
 			    </div>
 			  </div>
 			`
 		})
+
+		//Output repos
+		document.getElementById('repos').innerHTML = output
 
 	}
 
